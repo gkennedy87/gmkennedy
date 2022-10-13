9d72f348c8067c6ea3e02390fb1aed6738a4df9d
Grant Kennedy <grant@gmkennedy.com>
2022-10-13 20:36:17 +0300

    fixed navbar styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,13 @@
-import styles from '../styles/Navbar.module.css'
+import styles from '../styles/navbar.module.css'
 import {useState} from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Drawer from '@mui/material/Drawer'
 
@@ -95,6 +92,7 @@ const Navbar = () => {
                         flexGrow: 1,
                         fontFamily: 'monospace',
                         fontWeight: 700,
+                        fontSize:'1.25rem',
                         letterSpacing: '.3rem',
                         color: 'inherit',
                         textDecoration: 'none',
