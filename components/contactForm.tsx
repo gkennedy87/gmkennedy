e05c9febf5601b6752f9cd9e578edace4e9c09c5
Grant Kennedy <grant@gmkennedy.com>
2022-11-23 15:09:52 +0900

    hotfix: fixed export compiling error on contact form component

---
+++ components/contactForm.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Box } from "@mui/system";
+
+const ContactForm = () => {
+    return (
+        <Box component='form'>
+            <>
+                <h1>Hello World</h1>
+            </>
+        </Box>
+    )
+}
+
+export default ContactForm;
